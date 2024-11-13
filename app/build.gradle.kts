1d1147f88214b70fb796e74fac635c869e4d4b8d
Alon <alonteper5@gmail.com>
2024-11-13 16:03:47 +0200

    working LogIn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.pitstopfrenzy"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -27,9 +28,10 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+
 }
 
 dependencies {
@@ -43,6 +45,10 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.google.android.gms:play-services-auth:21.2.0")
-
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+}
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
